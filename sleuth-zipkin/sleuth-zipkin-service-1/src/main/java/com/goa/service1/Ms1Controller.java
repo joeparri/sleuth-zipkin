5749bc4c7110a33d295781366eb39908d63bcd5f
E016800 <E016800@iberdrola.com>
2019-02-04 11:48:08 +0100

    Se actualiza

--- sleuth-zipkin/sleuth-zipkin-service-1/src/main/java/com/goa/service1/Ms1Controller.java
+++ sleuth-zipkin/sleuth-zipkin-service-1/src/main/java/com/goa/service1/Ms1Controller.java
@@ -3,15 +3,11 @@ package com.goa.service1;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import brave.Tracer;
 
 
 @RestController
@@ -20,17 +16,7 @@ public class Ms1Controller {
 	private static final Logger LOG = Logger.getLogger(Ms1Controller.class.getName());
 
 	@Autowired
-	RestTemplate restTemplate;
-	
-	@Autowired
-	private Tracer tracer;
-	
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-	
+	private RestTemplate restTemplate;
 	
 	@GetMapping(value="/sayHello")
 	public String sayHello() {
@@ -40,7 +26,6 @@ public class Ms1Controller {
 		response = response.concat((String) restTemplate.exchange("http://service-2/sayHello", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
 	        }).getBody());
 		
-		response = response.concat(tracer.currentSpan().context().traceIdString());
 		 
 		return response;
 	}
